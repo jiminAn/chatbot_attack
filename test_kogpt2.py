ce0aab1f15d81604f3645ab1c4830c0572357d02
jiminAn <als398@naver.com>
2022-04-05 14:27:05 +0900

    Feat: generate sentence by test question

--- test_kogpt2.py
+++ test_kogpt2.py
@@ -1,13 +1,11 @@
 import torch
 from dialogLM.Kogpt2 import DialogKoGPT2
 from kogpt2_transformers import get_kogpt2_tokenizer
-save_ckpt_path = './dialogLM/checkpoint/kogpt2-data2_0_ep5.pth'
-
 assert torch.cuda.is_available()
-
 ctx = "cuda" if torch.cuda.is_available() else "cpu"
 device = torch.device(ctx)
 
+save_ckpt_path = './dialogLM/checkpoint/kogpt_wellness_epoch5_batch2.pth'
 model = DialogKoGPT2()
 checkpoint = torch.load(save_ckpt_path, map_location=device)
 model.load_state_dict(checkpoint['model_state_dict'])
@@ -15,7 +13,13 @@ model.eval()
 tokenizer = get_kogpt2_tokenizer()
 output_size = 200
 
-questions = ["요즘 너무 힘들어", "계속 머리가 빠지는 것 같아", "죽고싶어", "오늘 정말 기쁜일이 있었어"]
+test_data_path = './dialogLM/data/wellness.user_chatbot.16k.test.q'
+questions = []
+with open(test_data_path, 'rt', encoding='UTF8') as f:
+    for line in f.readlines():
+        q, a = line.split('\t')
+        questions.append(q.strip())
+
 t_idxs = [tokenizer.encode(q) for q in questions]
 input_idxs = [torch.tensor([tokenizer.bos_token_id,]+t_idx+[tokenizer.eos_token_id]).unsqueeze(0) for t_idx in t_idxs]
 
