6143777458f49d7151114021495131c2459bd1ce
jiminAn <als398@naver.com>
2022-04-05 15:22:38 +0900

    Feat: reduce time complexitt

--- test_kogpt2.py
+++ test_kogpt2.py
@@ -1,7 +1,9 @@
 import torch
+import time
 from dialogLM.Kogpt2 import DialogKoGPT2
 from kogpt2_transformers import get_kogpt2_tokenizer
 assert torch.cuda.is_available()
+start = time.time()  # 시작 시간 저장
 ctx = "cuda" if torch.cuda.is_available() else "cpu"
 device = torch.device(ctx)
 
@@ -13,17 +15,12 @@ model.eval()
 tokenizer = get_kogpt2_tokenizer()
 output_size = 200
 
-test_data_path = './dialogLM/data/wellness.user_chatbot.16k.test.ans'
-questions = []
+test_data_path = './dialogLM/data/wellness.user_chatbot.16k.test.q'
+
 with open(test_data_path, 'rt', encoding='UTF8') as f:
-    for line in f.readlines():
-        q, a = line.split('\t')
-        questions.append(q.strip())
+    for q in f.readlines():
+        t_idx = tokenizer.encode(q.strip())
+        result = model.generate(input_ids=torch.tensor([tokenizer.bos_token_id,]+t_idx+[tokenizer.eos_token_id]).unsqueeze(0))
+        print(tokenizer.decode(result[0].tolist()[len(t_idx)+1:],skip_special_tokens=True))
 
-t_idxs = [tokenizer.encode(q) for q in questions]
-input_idxs = [torch.tensor([tokenizer.bos_token_id,]+t_idx+[tokenizer.eos_token_id]).unsqueeze(0) for t_idx in t_idxs]
-
-for input_idx, t_idx in zip(input_idxs,t_idxs):
-    result = model.generate(input_ids=input_idx)
-    answer = tokenizer.decode(result[0].tolist()[len(t_idx)+1:],skip_special_tokens=True)
-    print(answer)
+print(time.time()-start)
